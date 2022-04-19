32b736e3608ea6055260debf6f5adedb676309fe
Mikhail Libchenko <libchenko44@mail.ru>
2022-04-19 03:03:25 +0300

    feat: first pair of equals expression analyzer and code fixer added

---
+++ AnalyzerTemplate/AnalyzerTemplate.CodeFixes/EqualsExpressionFixProvider.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.Composition;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.FindSymbols;
+using Microsoft.CodeAnalysis.Operations;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+using Document = Microsoft.CodeAnalysis.Document;
+using TypeInfo = Microsoft.CodeAnalysis.TypeInfo;
+
+namespace AnalyzerTemplate
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(EqualsExpressionFixProvider)), Shared]
+    public class EqualsExpressionFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds =>
+            ImmutableArray.Create(EqualsExpressionAnalyzer.DiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var diagnostic = context.Diagnostics.First();
+
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: CodeFixResources.CodeFixTitle,
+                    createChangedDocument: c => FixEqualsExpression(context.Document, diagnostic, root),
+                    equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
+                diagnostic);
+        }
+
+        private async Task<Document> FixEqualsExpression(Document document, Diagnostic diagnostic, SyntaxNode root)
+        {
+            var semanticModel = await document.GetSemanticModelAsync().ConfigureAwait(false);
+
+            var equalsBinaryExpressionSyntax = root.FindNode(diagnostic.Location.SourceSpan)
+                .FirstAncestorOrSelf<BinaryExpressionSyntax>();
+
+            var left = equalsBinaryExpressionSyntax.Left;
+            var right = equalsBinaryExpressionSyntax.Right;
+
+            var equalsOperation = semanticModel.GetOperation(equalsBinaryExpressionSyntax);
+
+            var leftTypeInfo = semanticModel.GetTypeInfo(left);
+            var rightTypeInfo = semanticModel.GetTypeInfo(right);
+
+            var leftSymbolInfo = semanticModel.GetSymbolInfo(left);
+            var rightSymbolInfo = semanticModel.GetSymbolInfo(right);
+
+            var leftType = leftTypeInfo.Type.TypeKind;
+            var rightType = leftTypeInfo.Type.TypeKind;
+
+            var ifLeftOverridesEquals = IfTypeOverridesMethod(leftTypeInfo, nameof(Equals));
+            var ifRightOverridesEquals = IfTypeOverridesMethod(rightTypeInfo, nameof(Equals));
+
+            if (ifRightOverridesEquals && ifLeftOverridesEquals)
+            {
+                var newExpression = InvocationExpression(
+                        MemberAccessExpression(
+                            SyntaxKind.SimpleMemberAccessExpression,
+                            IdentifierName(leftSymbolInfo.Symbol.Name),
+                            IdentifierName(nameof(Equals))))
+                    .WithArgumentList(
+                        ArgumentList(
+                            SingletonSeparatedList(
+                                Argument(
+                                    IdentifierName(rightSymbolInfo.Symbol.Name)))));
+
+                var newRoot = root.ReplaceNode(equalsOperation.Syntax, newExpression);
+
+                return await Task.FromResult(document.WithSyntaxRoot(newRoot));
+            }
+
+            return await Task.FromResult(document);
+        }
+
+        public bool IfTypeOverridesMethod(TypeInfo typeInfo, string methodName)
+        {
+            var typeMethods = typeInfo.Type.GetMembers();
+
+            if (typeMethods.IsDefaultOrEmpty) return false;
+
+            var equalsMethod = typeMethods.SingleOrDefault(m => m.Name == methodName) as IMethodSymbol;
+
+            return equalsMethod?.IsOverride ?? false;
+        }
+
+        public bool Overrides(MethodInfo baseMethod, Type type)
+        {
+            if (baseMethod == null) return false;
+            if (type == null) return false;
+            if (!type.IsSubclassOf(baseMethod.ReflectedType)) return false;
+
+            while (type != baseMethod.ReflectedType)
+            {
+                var methods = type.GetMethods(BindingFlags.Instance |
+                                              BindingFlags.DeclaredOnly |
+                                              BindingFlags.Public |
+                                              BindingFlags.NonPublic);
+
+                if (methods.Any(m => m.GetBaseDefinition() == baseMethod))
+                    return true;
+
+                type = type.BaseType;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ AnalyzerTemplate/AnalyzerTemplate/EqualsExpressionAnalyzer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AnalyzerTemplate
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class EqualsExpressionAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "BadEqualsExpression";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Unsafe equals expression";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+            context.RegisterSyntaxNodeAction(
+                AnalyzeEqualsExpression,
+                SyntaxKind.EqualsExpression);
+        }
+
+        public static void AnalyzeEqualsExpression(SyntaxNodeAnalysisContext context)
+        {
+/*            var semanticModel = context.SemanticModel;*/
+
+            if (!(context.Node is BinaryExpressionSyntax equalsBinaryExpressionSyntax)) return;
+
+            var left = equalsBinaryExpressionSyntax.Left;
+            var right = equalsBinaryExpressionSyntax.Right;
+
+/*            var leftTypeInfo = semanticModel.GetTypeInfo(left);
+            var rightTypeInfo = semanticModel.GetTypeInfo(right);
+
+            var leftType = leftTypeInfo.Type.TypeKind;
+            var rightType = rightTypeInfo.Type.TypeKind;
+
+            if (leftType != rightType) return;*/
+
+            var equalsOperation = context.SemanticModel.GetOperation(equalsBinaryExpressionSyntax);
+
+            var leftSymbolInfo = context.SemanticModel.GetPreprocessingSymbolInfo(left);
+            var rightSymbolInfo = context.SemanticModel.GetPreprocessingSymbolInfo(right);
+
+            context.ReportDiagnostic(Diagnostic
+                .Create(Rule, equalsBinaryExpressionSyntax.GetLocation(), Category));
+        }
+    }
+}
