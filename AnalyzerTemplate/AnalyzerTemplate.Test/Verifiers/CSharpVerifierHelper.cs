7a309127fd663e8c0920d1c6fbf0eb19a8afda79
Mikhail Libchenko <libchenkomikhail@gmail.com>
2022-04-23 01:39:38 +0300

    chore: diff made smaller by reverting unused changes

--- AnalyzerTemplate/AnalyzerTemplate.Test/Verifiers/CSharpVerifierHelper.cs
+++ AnalyzerTemplate/AnalyzerTemplate.Test/Verifiers/CSharpVerifierHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
-namespace AnalyzerTemplate.Test.Verifiers
+namespace AnalyzerTemplate.Test
 {
     internal static class CSharpVerifierHelper
     {
--- AnalyzerTemplate/AnalyzerTemplate.Test/Verifiers/VisualBasicCodeRefactoringVerifier`1.cs
+++ AnalyzerTemplate/AnalyzerTemplate.Test/Verifiers/VisualBasicCodeRefactoringVerifier`1.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.Testing;
 
-namespace AnalyzerTemplate.Test.Verifiers
+namespace AnalyzerTemplate.Test
 {
     public static partial class VisualBasicCodeRefactoringVerifier<TCodeRefactoring>
         where TCodeRefactoring : CodeRefactoringProvider, new()
