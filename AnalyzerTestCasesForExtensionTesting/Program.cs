39449b21a7e94cd7ce907a8064cb124fc071ed63
mLibchenko <mLibchenko@hazeltree.com>
2022-04-24 21:47:40 +0300

    chore: net version downgraded to net5.0

--- AnalyzerTestCasesForExtensionTesting/Program.cs
+++ AnalyzerTestCasesForExtensionTesting/Program.cs
@@ -1,93 +1,95 @@
-﻿namespace AnalyzerTestCases;
+﻿using System.Collections.Generic;
 
-public class Program
+namespace AnalyzerTestCases
 {
-    public interface IAboba { }
-
-    public class OverridenOperatorBase
+    public class Program
     {
-        public static bool operator ==(OverridenOperatorBase a, OverridenOperatorBase b) => false;
-        public static bool operator !=(OverridenOperatorBase a, OverridenOperatorBase b) => true;
-    }
+        public interface IAboba { }
 
-    public class NotOverridenOperatorInherited : OverridenOperatorBase { }
-    public class NotOverridenOperator { }
-
-    public class StudentWithEqualsOverride
-    {
-        public int Age { get; set; }
-
-        public static bool operator ==(StudentWithEqualsOverride a, StudentWithEqualsOverride b) => false;
-        public static bool operator !=(StudentWithEqualsOverride a, StudentWithEqualsOverride b) => true;
-        public override bool Equals(object? obj)
+        public class OverridenOperatorBase
         {
-            return false;
+            public static bool operator ==(OverridenOperatorBase a, OverridenOperatorBase b) => false;
+            public static bool operator !=(OverridenOperatorBase a, OverridenOperatorBase b) => true;
         }
-    }
 
-    public class StudentWithoutEqualsOverride<T>
-    {
-        public T Age { get; set; }
-    }
+        public class NotOverridenOperatorInherited : OverridenOperatorBase { }
+        public class NotOverridenOperator { }
 
-    public static void Main()
-    {
-    }
+        public class StudentWithEqualsOverride
+        {
+            public int Age { get; set; }
 
-    public static StudentWithoutEqualsOverride<int> StudentReturnStatement()
-    {
-        return null;
-    }
+            public static bool operator ==(StudentWithEqualsOverride a, StudentWithEqualsOverride b) => false;
+            public static bool operator !=(StudentWithEqualsOverride a, StudentWithEqualsOverride b) => true;
+            public override bool Equals(object? obj)
+            {
+                return false;
+            }
+        }
 
-    public static List<int> ListReturnStatement()
-    {
-        return null;
-    }
+        public class StudentWithoutEqualsOverride<T>
+        {
+            public T Age { get; set; }
+        }
 
-    public static IEnumerable<int> ListRet1urnStatement()
-    {
-        return null;
-    }
+        public static void Main()
+        {
+        }
 
-    public static IEnumerable<IEnumerable<IEnumerable<int>>> ListRet11urnStatement()
-    {
-        yield return null;
-    }
+        public static StudentWithoutEqualsOverride<int> StudentReturnStatement()
+        {
+            return null;
+        }
 
-    public static IEnumerator<int> ListRet1urnStsdfatement()
-    {
-        return null;
-    }
+        public static List<int> ListReturnStatement()
+        {
+            return null;
+        }
 
-    public static ICollection<int> ListR2eturnStatement()
-    {
-        return null;
-    }
+        public static IEnumerable<int> ListRet1urnStatement()
+        {
+            return null;
+        }
 
-    public static int[] ArrayReturnStatement()
-    {
-        return null;
-    }
+        public static IEnumerable<IEnumerable<IEnumerable<int>>> ListRet11urnStatement()
+        {
+            yield return null;
+        }
 
-    public static IEnumerable<List<int>> YieldIEnumerableReturnStatement()
-    {
-        return null;
-    }
+        public static IEnumerator<int> ListRet1urnStsdfatement()
+        {
+            return null;
+        }
 
-    public static IEnumerable<int[]> YieldIEnumerableReturnStatement1()
-    {
-        yield return null;
-    }
+        public static ICollection<int> ListR2eturnStatement()
+        {
+            return null;
+        }
 
-    public static IEnumerator<List<int>> YieldIEnumeratorReturnStatement()
-    {
-        yield return null;
-    }
+        public static int[] ArrayReturnStatement()
+        {
+            return null;
+        }
 
-    public static IEnumerable<IEnumerable<int>> YieldIEnumeratorReturnStatement1()
-    {
-        yield return null;
-    }
+        public static IEnumerable<List<int>> YieldIEnumerableReturnStatement()
+        {
+            return null;
+        }
+
+        public static IEnumerable<int[]> YieldIEnumerableReturnStatement1()
+        {
+            yield return null;
+        }
+
+        public static IEnumerator<List<int>> YieldIEnumeratorReturnStatement()
+        {
+            yield return null;
+        }
+
+        public static IEnumerable<IEnumerable<int>> YieldIEnumeratorReturnStatement1()
+        {
+            yield return null;
+        }
 
 /*    public static bool IfEqualWithOneOverride(StudentWithoutEqualsOverride<int> s1, StudentWithEqualsOverride s2) => s1 == s2;
 
@@ -101,10 +103,11 @@ public class Program
 
     public static bool IfEqualOneWithOverride(StudentWithoutEqualsOverride<int> s1, StudentWithEqualsOverride s2) => s1 == s2;*/
 
-    public static bool IfEqualOneWithOverride(OverridenOperatorBase s1, NotOverridenOperatorInherited s2) => s1 == s2;
-    public static bool IfEqualOneWithOverride(NotOverridenOperatorInherited s1, OverridenOperatorBase s2) => s1 == s2;
-    public static bool IfEqualOneWithOverride(NotOverridenOperator s1, NotOverridenOperator s2) => s1 == s2;
-    public static bool IfEqualOneWithOverride(NotOverridenOperator s1, object s2) => s1 == s2;
+        public static bool IfEqualOneWithOverride(OverridenOperatorBase s1, NotOverridenOperatorInherited s2) => s1 == s2;
+        public static bool IfEqualOneWithOverride(NotOverridenOperatorInherited s1, OverridenOperatorBase s2) => s1 == s2;
+        public static bool IfEqualOneWithOverride(NotOverridenOperator s1, NotOverridenOperator s2) => s1 == s2;
+        public static bool IfEqualOneWithOverride(NotOverridenOperator s1, object s2) => s1 == s2;
 
-    public static bool IfEqualOneWithOverride(IAboba s1, IAboba s2) => s1 == s2;
+        public static bool IfEqualOneWithOverride(IAboba s1, IAboba s2) => s1 == s2;
+    }
 }
